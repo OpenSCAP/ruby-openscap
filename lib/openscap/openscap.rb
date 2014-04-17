a1703e83d1a81c1359fd4407bf9ba0ce79b17763
Simon Lukasik <slukasik@redhat.com>
2014-04-17 20:39:14 +0200

    Introduce functions to query OpenSCAP's errno

--- lib/openscap/openscap.rb
+++ lib/openscap/openscap.rb
@@ -15,7 +15,20 @@ module OpenSCAP
   extend FFI::Library
   ffi_lib 'openscap'
 
+  def self.error?
+    return oscap_err()
+  end
+
+  def self.get_full_error
+    err = oscap_err_get_full_error
+    return err.null? ? nil : err.read_string()
+  end
+
   attach_function :oscap_init, [], :void
   attach_function :oscap_cleanup, [], :void
   attach_function :oscap_get_version, [], :string
+
+  attach_function :oscap_err, [], :bool
+  attach_function :oscap_err_get_full_error, [], :pointer
+  private_class_method :oscap_err, :oscap_err_get_full_error
 end

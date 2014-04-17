bb1dfcc45b23a274b3d66fbd652466364b10e0df
Simon Lukasik <slukasik@redhat.com>
2014-04-17 11:02:40 +0200

    Wrap-up the very first OpenSCAP functions

---
+++ lib/openscap/openscap.rb
@@ -0,0 +1,21 @@
+#
+# Copyright (c) 2014 Red Hat Inc.
+#
+# This software is licensed to you under the GNU General Public License,
+# version 2 (GPLv2). There is NO WARRANTY for this software, express or
+# implied, including the implied warranties of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+# along with this software; if not, see
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+#
+
+require 'ffi'
+
+module OpenSCAP
+  extend FFI::Library
+  ffi_lib 'openscap'
+
+  attach_function :oscap_init, [], :void
+  attach_function :oscap_cleanup, [], :void
+  attach_function :oscap_get_version, [], :string
+end

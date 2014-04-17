6e3b5982b881e8aeb4b1de18a37fad99d196161a
Simon Lukasik <slukasik@redhat.com>
2014-04-17 12:19:08 +0200

    test: That oscap_get_version is properly wrapped.

---
+++ test/openscap_test.rb
@@ -0,0 +1,22 @@
+#
+# Copyright (c) 2014 Red Hat Inc.
+#
+# This software is licensed to you under the GNU General Public License,
+# version 2 (GPLv2). There is NO WARRANTY for this software, express or
+# implied, including the implied warranties of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+# along with this software; if not, see
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+#
+
+require 'test/unit'
+require 'openscap'
+
+class TestOscapVersion < Test::Unit::TestCase
+  def test_oscap_version
+    OpenSCAP.oscap_init
+    version = OpenSCAP.oscap_get_version
+    OpenSCAP.oscap_cleanup
+    assert version.include?(".")
+  end
+end

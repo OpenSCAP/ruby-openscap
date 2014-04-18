ff7879ce51545dbf77614c3d5e4bca98c8468ae8
Simon Lukasik <slukasik@redhat.com>
2014-04-18 16:19:50 +0200

    OpenSCAP::Xccdf::Session.export_results

--- lib/openscap/xccdf/session.rb
+++ lib/openscap/xccdf/session.rb
@@ -33,14 +33,40 @@ module OpenSCAP
         end
       end
 
+      def export_results(rds_file=nil, xccdf_file=nil, report_file=nil,
+        oval_results=false, oval_variables=false, engines_results=false)
+        OpenSCAP.raise! unless OpenSCAP.xccdf_session_set_arf_export(@s, rds_file)
+        OpenSCAP.raise! unless OpenSCAP.xccdf_session_set_xccdf_export(@s, xccdf_file)
+        OpenSCAP.raise! unless OpenSCAP.xccdf_session_set_report_export(@s, report_file)
+        OpenSCAP.xccdf_session_set_oval_results_export(@s, oval_results)
+        OpenSCAP.xccdf_session_set_oval_variables_export(@s, oval_variables)
+        OpenSCAP.xccdf_session_set_check_engine_plugins_results_export(@s, engines_results)
+
+        OpenSCAP.raise! unless OpenSCAP.xccdf_session_export_oval(@s) == 0
+        OpenSCAP.raise! unless OpenSCAP.xccdf_session_export_check_engine_plugins(@s) == 0
+        OpenSCAP.raise! unless OpenSCAP.xccdf_session_export_xccdf(@s) == 0
+        OpenSCAP.raise! unless OpenSCAP.xccdf_session_export_arf(@s) == 0
+      end
+
       def finalize(s)
         OpenSCAP.xccdf_session_free(s.s)
       end
     end
-
   end
+
   attach_function :xccdf_session_new, [:string], :pointer
   attach_function :xccdf_session_free, [:pointer], :void
   attach_function :xccdf_session_load, [:pointer], :int
   attach_function :xccdf_session_evaluate, [:pointer], :int
+  attach_function :xccdf_session_export_oval, [:pointer], :int
+  attach_function :xccdf_session_export_check_engine_plugins, [:pointer], :int
+  attach_function :xccdf_session_export_xccdf, [:pointer], :int
+  attach_function :xccdf_session_export_arf, [:pointer], :int
+
+  attach_function :xccdf_session_set_arf_export, [:pointer, :string], :bool
+  attach_function :xccdf_session_set_xccdf_export, [:pointer, :string], :bool
+  attach_function :xccdf_session_set_report_export, [:pointer, :string], :bool
+  attach_function :xccdf_session_set_oval_variables_export, [:pointer, :bool], :void
+  attach_function :xccdf_session_set_oval_results_export, [:pointer, :bool], :void
+  attach_function :xccdf_session_set_check_engine_plugins_results_export, [:pointer, :bool], :void
 end

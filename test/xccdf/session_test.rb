4c648be6bf3ef08b159683edcd43f27a563de1aa
Simon Lukasik <slukasik@redhat.com>
2014-06-04 14:16:15 +0200

    test: Create xccdf_session out of nil

--- test/xccdf/session_test.rb
+++ test/xccdf/session_test.rb
@@ -24,6 +24,17 @@ class TestSession < OpenSCAP::TestCase
     assert msg.start_with?("Unable to open file: ''"), "Message was: " + msg
   end
 
+  def test_session_new_nil
+    msg = nil
+    begin
+      s = OpenSCAP::Xccdf::Session.new(nil)
+      assert false
+    rescue OpenSCAP::OpenSCAPError => e
+      msg = e.to_s
+    end
+    assert msg.start_with?("No filename specified!"), "Message was: " + msg
+  end
+
   def test_sds_false
     s = OpenSCAP::Xccdf::Session.new("../data/xccdf.xml")
     assert ! s.sds?

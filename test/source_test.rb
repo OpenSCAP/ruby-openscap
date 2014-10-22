009e9f6c53850383ae262530faf368f36f26fd25
Šimon Lukašík <slukasik@redhat.com>
2014-10-22 12:23:45 +0200

    Introduce a simple test for OpenSCAP::Source

---
+++ test/source_test.rb
@@ -0,0 +1,32 @@
+#
+# Copyright (c) 2014 Red Hat Inc.
+#
+# This software is licensed to you under the GNU General Public License,
+# version 2 (GPLv2). There is NO WARRANTY for this software, express or
+# implied, including the implied warranties of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+# along with this software; if not, see
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+#
+
+require 'openscap'
+require 'openscap/source'
+require 'common/testcase'
+
+class TestSession < OpenSCAP::TestCase
+  def test_source_new_nil
+    msg = nil
+    begin
+      s = OpenSCAP::Source.new(nil)
+      assert false
+    rescue OpenSCAP::OpenSCAPError => e
+      msg = e.to_s
+    end
+    assert msg.start_with?("No filename specified!"), "Message was: " + msg
+  end
+
+  def test_source_new_ok
+    s = OpenSCAP::Source.new("data/xccdf.xml")
+    s.destroy
+  end
+end

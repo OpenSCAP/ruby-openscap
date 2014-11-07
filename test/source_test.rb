16196dd7719250fe3079a4f16244cbe32d22dd2a
Šimon Lukašík <slukasik@redhat.com>
2014-11-07 16:45:10 +0100

    Tests: OpenSCAP::Source.validate

--- test/source_test.rb
+++ test/source_test.rb
@@ -40,18 +40,39 @@ class TestSource < OpenSCAP::TestCase
   def test_type_xccdf
     s = OpenSCAP::Source.new("../data/xccdf.xml")
     assert s.type == 'XCCDF Checklist', "Type was #{s.type}"
+    s.validate!
     s.destroy
   end
 
   def test_type_sds
     s = OpenSCAP::Source.new("../data/sds-complex.xml")
     assert s.type == 'SCAP Source Datastream', "Type was #{s.type}"
+    s.validate!
     s.destroy
   end
 
   def test_type_test_result
     s = OpenSCAP::Source.new("../data/testresult.xml")
     assert s.type == 'XCCDF Checklist', "Type was #{s.type}"
+    s.validate!
+    s.destroy
+  end
+
+  def test_validate_invalid
+    s = OpenSCAP::Source.new("../data/invalid.xml")
+    msg = nil
+    begin
+      s.validate!
+      assert false
+    rescue OpenSCAP::OpenSCAPError => e
+      msg = e.to_s
+    end
+    assert msg.start_with?('Invalid XCCDF Checklist (1.2) content in ../data/invalid.xml.'),
+      "Message was: " + msg
+    assert msg.include?("../data/invalid.xml:3: Element '{http"),
+      "Message was: " + msg
+    assert msg.include?("This element is not expected. Expected is"),
+      "Message was: " + msg
     s.destroy
   end
 end

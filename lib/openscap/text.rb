672932ba4a1c02b2e4a27891288e6f692e735e96
Šimon Lukašík <slukasik@redhat.com>
2014-11-13 12:57:35 +0100

    Introduce: OpenSCAP::TextList

--- lib/openscap/text.rb
+++ lib/openscap/text.rb
@@ -31,8 +31,25 @@ module OpenSCAP
     end
   end
 
+  class TextList
+    def initialize(oscap_text_iterator)
+      @raw = oscap_text_iterator
+    end
+
+    def plaintext(lang=nil)
+      OpenSCAP.oscap_textlist_get_preferred_plaintext @raw, lang
+    end
+
+    def destroy
+      OpenSCAP.oscap_text_iterator_free @raw
+    end
+  end
+
   attach_function :oscap_text_new, [], :pointer
   attach_function :oscap_text_set_text, [:pointer, :string], :bool
   attach_function :oscap_text_get_text, [:pointer], :string
   attach_function :oscap_text_free, [:pointer], :void
+
+  attach_function :oscap_textlist_get_preferred_plaintext, [:pointer, :string], :string
+  attach_function :oscap_text_iterator_free, [:pointer], :void
 end

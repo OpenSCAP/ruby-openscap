3b16342182d7071175ea09bb7596cd8a3a2c22c5
Šimon Lukašík <slukasik@redhat.com>
2015-08-26 15:37:47 +0200

    Introduce: OpenSCAP::Xccdf::Value

---
+++ lib/openscap/xccdf/value.rb
@@ -0,0 +1,20 @@
+#
+# Copyright (c) 2015 Red Hat Inc.
+#
+# This software is licensed to you under the GNU General Public License,
+# version 2 (GPLv2). There is NO WARRANTY for this software, express or
+# implied, including the implied warranties of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+# along with this software; if not, see
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+#
+
+require 'openscap/exceptions'
+require 'openscap/xccdf/item'
+
+module OpenSCAP
+  module Xccdf
+    class Value < Item
+    end
+  end
+end

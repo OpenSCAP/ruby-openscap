3c2eb7e16c6a83da24365d70ac2eb666618bbf5c
Shlomi Zadok <shlomi@ben-hanna.com>
2015-09-06 10:58:20 +0300

    Introduce Rule.Fix and Xccdf::Fix

---
+++ lib/openscap/xccdf/fix.rb
@@ -0,0 +1,41 @@
+module OpenSCAP
+  module Xccdf
+    class Fix
+      def initialize(raw)
+        fail OpenSCAP::OpenSCAPError, "Cannot initialize OpenSCAP::Xccdf::Reference with '#{raw}'" \
+          unless raw.is_a?(FFI::Pointer)
+        @raw = raw
+      end
+
+      def id
+        OpenSCAP.xccdf_fix_get_id(@raw)
+      end
+
+      def platform
+        OpenSCAP.xccdf_fix_get_platform(@raw)
+      end
+
+      # system is a reserved word in Rails, so didn't use it
+      def get_system
+        OpenSCAP.xccdf_fix_get_system(@raw)
+      end
+
+      def content
+        OpenSCAP.xccdf_fix_get_content(@raw)
+      end
+
+      def to_hash
+        {
+          :id => id,
+          :platform => platform,
+          :system => get_system,
+          :content => content
+        }
+      end
+    end
+  end
+  attach_function :xccdf_fix_get_id, [:pointer], :string
+  attach_function :xccdf_fix_get_platform, [:pointer], :string
+  attach_function :xccdf_fix_get_system, [:pointer], :string
+  attach_function :xccdf_fix_get_content, [:pointer], :string
+end

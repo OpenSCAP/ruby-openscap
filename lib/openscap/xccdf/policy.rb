681ced26863c22f52dc09953ea8928d60a178ee6
Šimon Lukašík <slukasik@redhat.com>
2016-02-24 23:09:27 +0100

    Introduce OpenSCAP::Xccdf::Policy

---
+++ lib/openscap/xccdf/policy.rb
@@ -0,0 +1,35 @@
+#
+# Copyright (c) 2016 Red Hat Inc.
+#
+# This software is licensed to you under the GNU General Public License,
+# version 2 (GPLv2). There is NO WARRANTY for this software, express or
+# implied, including the implied warranties of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+# along with this software; if not, see
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+#
+
+require 'openscap/exceptions'
+
+module OpenSCAP
+  module Xccdf
+    class Policy
+      def initialize(p)
+        case p
+        when FFI::Pointer
+          @raw = p
+        else
+          fail OpenSCAP::OpenSCAPError,
+               "Cannot initialize OpenSCAP::Xccdf::Policy with '#{p}'"
+        end
+        OpenSCAP.raise! if @raw.null?
+      end
+
+      def id
+        OpenSCAP.xccdf_policy_get_id raw
+      end
+    end
+  end
+
+  attach_function :xccdf_policy_get_id, [:pointer], :string
+end

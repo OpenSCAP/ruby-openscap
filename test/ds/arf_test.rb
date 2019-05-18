a7d6bee5df8ab945062602c7e0afe19c83f8c3d9
Šimon Lukašík <isimluk@fedoraproject.org>
2019-05-18 17:20:34 +0200

    Fix rubocop Style/FrozenStringLiteralComment
    Addressing:
    C: Style/FrozenStringLiteralComment: Missing magic comment # frozen_string_literal: true

--- test/ds/arf_test.rb
+++ test/ds/arf_test.rb
@@ -1,20 +1,11 @@
-#
-# Copyright (c) 2014 Red Hat Inc.
-#
-# This software is licensed to you under the GNU General Public License,
-# version 2 (GPLv2). There is NO WARRANTY for this software, express or
-# implied, including the implied warranties of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
-# along with this software; if not, see
-# http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
-#
+# frozen_string_literal: true
 
 require 'openscap'
 require 'openscap/ds/arf'
 require 'common/testcase'
 
 class TestArf < OpenSCAP::TestCase
-  REPORT = 'report.rds.xml'.freeze
+  REPORT = 'report.rds.xml'
 
   def test_arf_new_nil
     msg = nil

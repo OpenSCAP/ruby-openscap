6d0f746cd6304e85cfe46aadd42787153a2e5701
Šimon Lukašík <isimluk@fedoraproject.org>
2023-05-24 12:15:52 +0200

    chores: renovate dependencies

--- runtest.sh
+++ runtest.sh
@@ -2,7 +2,7 @@ set -e -o pipefail
 set -x
 
 rm -f openscap-*.gem
-gem build openscap.gemspec
-gem install openscap-*.gem
-rake test
-rubocop
+#gem build openscap.gemspec
+#gem install openscap-*.gem
+bundle exec rake test
+bundle exec rubocop
